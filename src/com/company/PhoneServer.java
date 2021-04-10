68dec8fe5673af323d8cbc6cdec69d35db9dafd1
Ryan OConnor <ryanoconnor010@gmail.com>
2021-04-10 16:30:46 -0400

    Implement the new thread handler class

--- src/com/company/PhoneServer.java
+++ src/com/company/PhoneServer.java
@@ -6,18 +6,22 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 import java.net.*;
-
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class PhoneServer {
 
     public static void main(String[] args) throws Exception {
 
-         /**
-         * Open a server socket on the specified port number (2014) and monitor the port
-         * for connection requests. When a connection request is received, create a client
-         * request thread, passing to its constructor a reference to the Socket object that
-         * represents the established connection with the client.
-         */
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+
+        /**
+        * Open a server socket on the specified port number (2014) and monitor the port
+        * for connection requests. When a connection request is received, create a client
+        * request thread, passing to its constructor a reference to the Socket object that
+        * represents the established connection with the client.
+        */
 
         System.out.println("Server listening");
 
@@ -25,9 +29,9 @@ public class PhoneServer {
         int my_port = 2014;
         ServerSocket listener = new ServerSocket(my_port);
 
-
-
-        while (true) {
+        // Begin infinite loop for server to wait for client connection requests
+        while (true)
+        {
 
             Socket clientSocket = null;
 
@@ -35,10 +39,17 @@ public class PhoneServer {
 
                 // Handle incoming client request
                 clientSocket = listener.accept();
-                System.out.println("Client has connected to PhoneServer 1.0 from: " + clientSocket);
+
+                LocalDateTime now = LocalDateTime.now();
+                System.out.println(dtf.format(now) + "-> Client has connected to Multi Threaded PhoneServer 1.0 from: " + clientSocket);
+
+                DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+                DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+
+                Thread thread = new ClientThread(clientSocket, dis, dos);
+                thread.start();
 
             }
-
             catch (Exception e) {
                 clientSocket.close();
                 e.printStackTrace();
@@ -55,5 +66,38 @@ public class PhoneServer {
         // new java.util.Scanner(System.in).nextLine();
 
 
+    }
+}
+
+class ClientThread extends Thread
+{
+    private final DataInputStream dis;
+    private final DataOutputStream dos;
+    Socket s;
+
+    // Constructor
+    public ClientThread(Socket s, DataInputStream dis, DataOutputStream dos)
+    {
+        this.s = s;
+        this.dis = dis;
+        this.dos = dos;
+
+    }
+
+    @Override
+    public void run()
+    {
+        String received;
+        String toreturn;
+
+        while(true) {
+            try {
+                dos.writeUTF("What do you want");
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }   // handleConnection(socket);
     }
 }
